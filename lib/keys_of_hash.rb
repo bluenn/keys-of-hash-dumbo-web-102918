8944dee2c26fea9e065b6cd126eec9bafe6d3cd9
a lan <bluenowte97@gmail.com>
2018-11-12 20:32:48 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,10 +1,12 @@
 class Hash
   def keys_of(*arguments)
     # code goes here
-   final = array.select do |key, arguments|
-    if array[key] == arguments
-      key
-    end
+     final = array.select do |key, arguments|
+       if array[key] == arguments
+         key
+        end
+      end
+     final
   end
-  final
 end
+
